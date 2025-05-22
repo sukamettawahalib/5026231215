da82a56076dfb6def4eba132a50449ddc489b1f5
sukamettawahalib <umaralhaddad1611@gmail.com>
2025-05-22 15:48:50 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+
 // import java.io ;
 
 // System.out.println("Hello World");
@@ -55,3 +59,15 @@ Route::get('delapan', function () {
 Route::get('ETS', function () {
     return view('atlsn');
 });
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('welcome', [DosenController::class, 'welcome']);
+
+Route::get('/pegawai/{name}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
